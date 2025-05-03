5c738bad4115f3c28557fbf60f6f497972552428
Aung Htwe <105806021+aung-htwe@users.noreply.github.com>
2025-05-02 23:47:40 -0500

    Update main.c

--- 3way-openmp/main.c
+++ 3way-openmp/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_LINES 1000000
 #define MAX_LINE_LENGTH 3000
@@ -25,78 +26,42 @@ int max_ascii(const char* str){
 
 /// Main program that implements parallel programming with OpenMP
 int main(){
-	// Open file
-	FILE *fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
-	if (fd == NULL)
-	{
-		fprintf(stderr, "ERROR: File not read\n");
-		return 1;
-	}
+	int max_values[MAX_LINES];
 
-
-
-	// Initialize array to store max ascii values
-	int* max_values = (int*) malloc(MAX_LINES * sizeof(int));
-	if (max_values == NULL){
-		fprintf(stderr, "Failed to allocate max_values\n");
-		return 1;
-	}
-
-
-
-	// array to hold all lines from file
-	char **lines = (char**)malloc(MAX_LINES * sizeof(char*));
-	if (lines == NULL){
-		printf("ERROR: Failed to allocate memory for lines\n");
-		return 1;
-	}
-
-	for (int i = 0; i < MAX_LINES; i++){
-		lines[i] = (char*) malloc(MAX_LINE_LENGTH * sizeof(char));
-		if (lines[i] == NULL){
-			printf("ERROR: Failed to allocate memory for lines\n");
-			return 1;
-		}
-	}
-
-
-	// Read all lines into array
-	int total_lines = 0;
-	while (total_lines < MAX_LINES && fgets(lines[total_lines], MAX_LINE_LENGTH, fd) != NULL){
-		lines[total_lines][strcspn(lines[total_lines], "\n")] = '\0';
-		total_lines++;
-	}
-	fclose(fd);
-
-
-
-	// Get number of threads and print
-	int num_threads = omp_get_num_threads();
-	fprintf(stdout, "Number of threads: %d\n", num_threads);
-
-	int partition = MAX_LINES / num_threads;
-
-	// ID of thread
 	int thread_id;
 
+	bool flag = false;
 
-
+	// Open file and calculate max in parallel
 	#pragma omp parallel private(thread_id)
 	{
-		// grab thread_id & print
 		thread_id = omp_get_thread_num();
 		fprintf(stdout, "Initializing thread %d\n", thread_id);
+		#pragma omp barrier
 
-
-		int i, max;
-
-		// Run the for loop in parallel that calculates the max ascii of each line
-		// No need for synchronization
-		#pragma omp for schedule(static, partition)
-		for (i = 0; i < total_lines; i++){ //for (i = thread_id * partition; i < (thread_id + 1) * partition - 1; i++){
-			max = max_ascii(lines[i]);
-			max_values[i] = max;
+		FILE *fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
+		if (fd == NULL)
+		{
+			fprintf(stderr, "ERROR: File not read\n");
 		}
+
+		char* line = (char*) malloc(MAX_LINE_LENGTH);
+		int line_num = 0;
+
+		#pragma omp for schedule(dynamic)
+		for (int i = 0; i < MAX_LINES; i++){
+			if (flag) continue;
+
+			if (fgets(line, MAX_LINE_LENGTH, fd) == NULL)
+				flag = true;
+
+			line[strcspn(line, "\n")] = '\0';
+			max_values[i] = max_ascii(line);
+		}
+
+		free(line);
+		fclose(fd);
+
 	}
 
 
@@ -105,12 +70,5 @@ int main(){
 		fprintf(stdout, "Line %d max ASCII: %d\n", i, max_values[i]);
 
 
-
-
-	for (int i = 0; i < MAX_LINES; i++)
-		free(lines[i]);
-
-	free(max_values);
-	free(lines);
 
 }
